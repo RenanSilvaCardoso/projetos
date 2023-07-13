22bb45f966b69bfa281db8dae23a499619768c44
renaxcardoso <81385406+RenanSilvaCardoso@users.noreply.github.com>
2023-07-12 22:14:01 -0300

    Melhoras na responsividade do site

--- projeto_arthur_bezerra/splide.js
+++ projeto_arthur_bezerra/splide.js
@@ -8,15 +8,20 @@ var splide = new Splide( '.splide', {
     snap: true,
     autoplay: true,
     breakpoints: {
+      768: {
+        perPage: 1,
+        gap    : '.7rem',
+        padding: '1.5rem',
+      },
       640: {
         perPage: 2,
         gap    : '.7rem',
         padding: '1.5rem',
       },
-      767: {
+      440: {
         perPage: 1,
         gap    : '.7rem',
-        padding: '1.5rem',
+        padding: '1rem',
       },
     },
   } );
